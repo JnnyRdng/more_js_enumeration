d2bfe3b27144f86f461636163a169e95e4d535c7
Jonny Reading <jonnyreading@gmail.com>
2020-08-19 16:16:22 +0100

    Code written and tests now passs. Pseudocode led me down the wrong path - don't try to modify an array as you loop over it! How many times... If last index of character is the same as the loop's current index, it's a unique character

--- hw_advanced_enumeration_start/isogram_finder/isogram_finder.js
+++ hw_advanced_enumeration_start/isogram_finder/isogram_finder.js
@@ -1,12 +1,17 @@
 const IsogramFinder = function (word) {
-    this.word = word;
+    this.word = word.toLowerCase().split("");
 }
 
 IsogramFinder.prototype.isIsogram = function () {
     // for every letter in word
-    // remove that letter from word by index
-    // test if letter still in word
-    // if it is, it's not an isogram
+    // ~~remove that letter from word by index~~
+    // ~~test if letter still in word~~
+    // ~~if it is, it's not an isogram~~
+    // scrap that.
+    // if index of letter === lastIndexOf(letter) it's a unique character
+    return this.word.every((letter, index, word) => {
+        return word.lastIndexOf(letter) === index;
+    });
 }
 
 module.exports = IsogramFinder;
