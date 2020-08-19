c3f3c02405e75a6a48c9528dab593f3eff83b0fa
Jonny Reading <jonnyreading@gmail.com>
2020-08-19 17:21:59 +0100

    code written to pass anagram tests

--- hw_advanced_enumeration_start/anagram_finder/anagram_finder.js
+++ hw_advanced_enumeration_start/anagram_finder/anagram_finder.js
@@ -1,9 +1,18 @@
 const AnagramFinder = function (word) {
-
+    this.word = word.toLowerCase();
 }
 
 AnagramFinder.prototype.findAnagrams = function (otherWords) {
-
+    // sort the original word alphabetically (eg. "cat" --> "act", "javascript" --> "aacijprstv")
+    // filter the given words:
+    // // sort each given word, if it matches the sorted original word, it's an anagram.
+    const sortedOriginalWord = this.word.split("").sort().join("");
+    return otherWords.filter((checkWord) => {
+        if (this.word !== checkWord.toLowerCase()) {
+            const sortedCheckWord = checkWord.toLowerCase().split("").sort().join("");
+            return sortedCheckWord === sortedOriginalWord;
+        }
+    });
 }
 
 module.exports = AnagramFinder;
