a1ddd65cc8f71959a2674223cbf4048692138ef3
Jonny Reading <jonnyreading@gmail.com>
2020-08-19 17:39:21 +0100

    finished traveller extras

--- hw_advanced_enumeration_start/hw_enumeration_traveller/hw_enumeration_traveller_start/models/traveller.js
+++ hw_advanced_enumeration_start/hw_enumeration_traveller/hw_enumeration_traveller_start/models/traveller.js
@@ -3,26 +3,30 @@ const Traveller = function (journeys) {
 };
 
 Traveller.prototype.getJourneyStartLocations = function () {
-
+  return this.journeys.map((journey) => journey.startLocation);
 };
 
 Traveller.prototype.getJourneyEndLocations = function () {
-
+  return this.journeys.map((journey) => journey.endLocation);
 };
 
 Traveller.prototype.getJourneysByTransport = function (transport) {
-
+  return this.journeys.filter(journey => journey.transport === transport)
 };
 
 Traveller.prototype.getJourneysByMinDistance = function (minDistance) {
-
+  return this.journeys.filter(journey => journey.distance > minDistance);
 };
 
 Traveller.prototype.calculateTotalDistanceTravelled = function () {
-
+  return this.journeys.reduce((total, journey) => total + journey.distance, 0);
 };
 
 Traveller.prototype.getUniqueModesOfTransport = function () {
+  const journeyTypes = this.journeys.map(journey => journey.transport);
+  return journeyTypes.filter((type, index, journeyTypes) => {
+    return index === journeyTypes.lastIndexOf(type);
+  })
 
 };
 
