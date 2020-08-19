3aaca1434dc08369d2eb29ffe4cac8aecbbb7cce
Jonny Reading <jonnyreading@gmail.com>
2020-08-19 15:54:47 +0100

    added pseudocode to isogram

--- hw_advanced_enumeration_start/isogram_finder/isogram_finder.js
+++ hw_advanced_enumeration_start/isogram_finder/isogram_finder.js
@@ -1,9 +1,12 @@
 const IsogramFinder = function (word) {
-
+    this.word = word;
 }
 
 IsogramFinder.prototype.isIsogram = function () {
-
+    // for every letter in word
+    // remove that letter from word by index
+    // test if letter still in word
+    // if it is, it's not an isogram
 }
 
 module.exports = IsogramFinder;
