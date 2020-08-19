39e0982e70e1d80926814040a5c020c9cbf31bd1
Jonny Reading <jonnyreading@gmail.com>
2020-08-19 15:49:27 +0100

    pangram finder passes tests

--- hw_advanced_enumeration_start/pangram_finder/pangram_finder.js
+++ hw_advanced_enumeration_start/pangram_finder/pangram_finder.js
@@ -1,9 +1,13 @@
 const PangramFinder = function (phrase) {
   this.alphabet = 'qwertyuiopasdfghjklzxcvbnm'.split('');
+  this.phrase = phrase;
 }
 
 PangramFinder.prototype.isPangram = function () {
-
+  searchSpace = this.phrase.toLowerCase();
+  return this.alphabet.every((letter) => {
+    return searchSpace.indexOf(letter) > -1;
+  });
 }
 
 module.exports = PangramFinder;
